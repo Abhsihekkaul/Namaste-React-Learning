63a8a7b99976e1c38747778df906716867b23bcc
Abhishek kaul <Abhishekkaul32@gmail.com>
2024-09-23 08:10:36 +0530

    Progress so far in react

---
+++ lecture1/app.js
@@ -0,0 +1,16 @@
+const heading1 = React.createElement("h1", {
+    id:"heading1",
+}, "Hello world 1");
+
+const heading2 = React.createElement("h2", {
+    id:"heading2",
+}, "Hello world 2");
+
+
+// here in order to provide it two child we have to use the arrays
+const container = React.createElement("div", {
+    class:"container-1",
+}, [heading1,heading2]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render([container]);
